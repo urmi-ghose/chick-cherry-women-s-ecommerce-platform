f26c96867b0cf421c8ade5d5cf7f81230cda1d81
IstiakAdil14 <istiakadil8@gmail.com>
2026-07-30 01:49:50 +0600

    Include custom admin views in main urls and revert sidebar links

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -26,7 +26,26 @@ from greatkart.admin_site import ChicCherryAdminSite
 admin.site.__class__ = ChicCherryAdminSite
 admin.site.logout_template = "accounts/logged_out.html"
 
+from . import urls_admin
+from django.shortcuts import redirect
+
 urlpatterns = [
+    path("secureadmin/manage-website/", urls_admin.admin_manage_website, name="admin_manage_website"),
+    path("secureadmin/users/", urls_admin.admin_users_list, name="admin_users_list"),
+    path("secureadmin/store/product/list/", urls_admin.admin_product_list, name="admin_product_list"),
+    path("secureadmin/store/product/", lambda request: redirect("/secureadmin/store/product/list/")),
+    path("secureadmin/category/list/", urls_admin.admin_category_list, name="admin_category_list"),
+    path("secureadmin/orders/", urls_admin.admin_order_list, name="admin_order_list"),
+    path("secureadmin/carts/order/", urls_admin.admin_order_list),
+    path("secureadmin/carts/order/<int:pk>/change/", urls_admin.admin_order_change, name="admin_order_change_custom"),
+    path("secureadmin/reviews/", urls_admin.admin_review_list, name="admin_review_list"),
+    path("secureadmin/store/reviewrating/", urls_admin.admin_review_list),
+    path("secureadmin/reviews/<int:pk>/approve/", urls_admin.admin_review_toggle, {'action': 'approve'}, name="admin_review_approve"),
+    path("secureadmin/reviews/<int:pk>/reject/",  urls_admin.admin_review_toggle, {'action': 'reject'},  name="admin_review_reject"),
+    path("secureadmin/store-preview/", urls_admin.admin_store_preview, name="admin_store_preview"),
+    path("secureadmin/discounts/", urls_admin.admin_discount_products, name="admin_discount_products"),
+    path("secureadmin/accounts/", lambda request: redirect("/secureadmin/accounts/account/")),
+
     path("secureadmin/", admin.site.urls),
     path("", views.home, name="home"),
     path("store/", include("store.urls")),
