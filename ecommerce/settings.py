b43099c3c1ddb2cad3b28ca4abb2f8835d407c0b
IstiakAdil14 <istiakadil8@gmail.com>
2026-07-30 01:09:42 +0600

    Fix template precedence to load ecommerce home.html

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = "ecommerce.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates", BASE_DIR / "ecommerce" / "templates"],
+        "DIRS": [BASE_DIR / "ecommerce" / "templates", BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
