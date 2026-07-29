9744ea2cef1b62f42ca6521541504c2d62c93d5e
IstiakAdil14 <istiakadil8@gmail.com>
2026-07-30 00:55:41 +0600

    Fix template path for production

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = "greatkart.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
